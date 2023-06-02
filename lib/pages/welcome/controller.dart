0dbd1d8d66e17d86202fbbc1f7bb14d5f58818ce
Badhon100 <badhonx102@gmail.com>
2023-06-03 00:47:52 +0600

    Welcome pages done

--- lib/pages/welcome/controller.dart
+++ lib/pages/welcome/controller.dart
@@ -5,4 +5,8 @@ import 'index.dart';
 class WelcomeController extends GetxController{
   final state = WelcomeState();
   WelcomeController();
+
+  changePage(int index) async{
+    state.index.value = index;
+  }
 }
--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,4 +1,5 @@
 
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -14,14 +15,14 @@ class WelcomePage extends GetView<WelcomeController> {
       body: SizedBox(
         width: 369.w,
         height: 780.h,
-        child: Stack(
+        child: Obx(()=>Stack(
           alignment: Alignment.bottomCenter,
           children: [
             PageView(
               scrollDirection: Axis.horizontal,
-              reverse: false,
+              reverse: true,
               onPageChanged: (index){
-
+                controller.state.index.value = index;
               },
               controller: PageController(
                 initialPage: 9, keepPage: false
@@ -37,6 +38,29 @@ class WelcomePage extends GetView<WelcomeController> {
                       fit: BoxFit.fill,
                       image: NetworkImage("https://i.pinimg.com/736x/bd/35/ca/bd35caa1d66da3a15ed952b4ac1010f7.jpg")
                     )
+                  ),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Positioned(
+                        bottom: 90,
+                        child: ElevatedButton(
+                          onPressed: (){},
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.white),
+                            foregroundColor: MaterialStateProperty.all(Colors.black),
+                            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                              )
+                            ),
+                            side: MaterialStateProperty.all(
+                              const BorderSide(color: Colors.white)
+                            )
+                          ),
+                          child: const Text("Login"),
+                        ) 
+                      ),
+                    ],
                   ),
                 ),
                 Container(
@@ -58,11 +82,29 @@ class WelcomePage extends GetView<WelcomeController> {
                       image: NetworkImage("https://marketplace.canva.com/EAE3NoV0pm0/1/0/900w/canva-live-in-the-moment-quote-sunset-floral-phone-wallpaper-mg3EIAS3gP0.jpg")
                     )
                   ),
+                  
                 ),
               ],
+            ),
+            Positioned(
+              bottom: 70,
+              child: DotsIndicator(
+                position: controller.state.index.value,
+                dotsCount: 3,
+                reversed: true,
+                mainAxisAlignment: MainAxisAlignment.center,
+                decorator: DotsDecorator(
+                  size: const Size.square(9),
+                  activeSize: const Size(19.0, 8.0),
+                  activeShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)
+                  )
+                ),
+              )
             )
           ],
         ),
+        )
       )
     );
   }
